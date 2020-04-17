ad0a4f269a697d8932a03e91922d19f32c50b1fd
gaurangrshah <gaurang.r.shah@gmail.com>
2020-04-17 01:21:52 -0400

    chore: adds iniital Movie / Actor models

--- models.py
+++ models.py
@@ -4,14 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 
-database_name = "cap-agency"
-# database_path = "postgres://{}:{}@{}/{}".format(
-#     'postgres', 'bunty', 'localhost:5432', database_name)
+database_name = "casting"
 
-database_url = "postgres://domxhljotpyuck:046e562a3389dd0888da6cc19cc4efcc20640f94f566aa0c8b262f0296b22008@ec2-34-200-116-132.compute-1.amazonaws.com:5432/d4ik1oq9u89jsb"
+heroku_db = "postgres://domxhljotpyuck:046e562a3389dd0888da6cc19cc4efcc20640f94f566aa0c8b262f0296b22008@ec2-34-200-116-132.compute-1.amazonaws.com:5432/d4ik1oq9u89jsb"
+# database_path = database_url
 
-# database_path = os.environ['DATABASE_URL']
-database_path = database_url
+local_db = "postgres://{}:{}@{}/{}".format(
+    'postgres', 'bunty', 'localhost:5432', database_name)
+
+# if os.environ['DATABASE_URL']:
+#     db_from_vars = os.environ['DATABASE_URL']
+
+database_path = local_db
+
 
 db = SQLAlchemy()
 
@@ -40,19 +45,40 @@ Have title and release year
 '''
 
 
-class Person(db.Model):
-    __tablename__ = 'People'
+class Actor(db.Model):
+    __tablename__ = 'actors'
 
     id = Column(db.Integer, primary_key=True)
     name = Column(db.String)
-    catchphrase = Column(db.String)
+    age = Column(db.Integer)
+    gender = Column(db.String)  # 🚧 create ENUM
 
-    def __init__(self, name, catchphrase=""):
+    def __init__(self, name, age, gender):
         self.name = name
-        self.catchphrase = catchphrase
+        self.age = age
+        self.gender = gender
 
     def format(self):
         return {
             'id': self.id,
             'name': self.name,
-            'catchphrase': self.catchphrase}
+            'age': self.age,
+            'gender': self.gender}
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(db.Integer, primary_key=True)
+    title = Column(db.String)
+    release_date = Column(db.String)
+
+    def __init__(self, name, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date}

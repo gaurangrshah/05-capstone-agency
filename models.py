a3d4a061e8371a761cfcda44fd010d2815efd95f
gaurangrshah <gaurang.r.shah@gmail.com>
2020-04-17 02:55:23 -0400

    test: deploy

--- models.py
+++ models.py
@@ -11,11 +11,11 @@ heroku_db = 'postgres://domxhljotpyuck:046e562a3389dd0888da6cc19cc4efcc20640f94f
 local_db = 'postgres://{}:{}@{}/{}'.format(
     'postgres', 'bunty', 'localhost:5432', database_name)
 
-# if os.environ['DATABASE_URL']:
-#     db_from_vars = os.environ['DATABASE_URL']
+
+# database_path = os.getenv('DATABASE_URL')
 
 # database_path = local_db
-database_path = os.getenv('DATABASE_URL')
+database_path = 'postgres://domxhljotpyuck:046e562a3389dd0888da6cc19cc4efcc20640f94f566aa0c8b262f0296b22008@ec2-34-200-116-132.compute-1.amazonaws.com:5432/d4ik1oq9u89jsb'
 # database_path = 'postgres://{}:{}@{}/{}'.format(
 #     'postgres', 'bunty', 'localhost:5432', database_name)
 

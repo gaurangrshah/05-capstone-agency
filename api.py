bb91743da7d9cf3f1b61515b7242788db76f0399
gaurangrshah <gaurang.r.shah@gmail.com>
2020-04-17 01:25:46 -0400

    chore: fix api syntax error

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 from models import db_drop_and_create_all, setup_db, db, Actor, Movie  # 🚨 IMPORT MODELS
 from auth import AuthError, requires_auth  # ✅ IMPORT AUTH
-# from app import app # 🚨 IMPORT APP
+from app import app  # 🚨 IMPORT APP
 
 '''
 @TODO uncomment the following line to initialize the datbase
@@ -57,6 +57,135 @@ def get_actors():
         'success': True,
         'actors': short_actor
     }), 200
+
+# '''
+# @TODO implement endpoint
+#     GET /drinks-detail
+#         it should require the 'get:drinks-detail' permission
+#         it should contain the drink.long() data representation
+#     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+#         or appropriate status code indicating reason for failure
+# '''
+# @app.route('/drinks-detail', methods=['GET'])
+# @requires_auth('get:drinks-detail')
+# def get_drinks_detail(jwt):
+#     """Returns a list of objects containing a long-form representation of Drinks """
+#     all_drinks = Drink.query.all()
+
+#     if all_drinks is None:
+#         abort(404, 'There are no drinks available')
+
+#     long_drink = [drink.long() for drink in all_drinks]
+
+#     return jsonify({
+#         'success': True,
+#         'drinks': long_drink
+#     }), 200
+
+
+# '''
+# @TODO implement endpoint
+#     POST /drinks
+#         it should create a new row in the drinks table
+#         it should require the 'post:drinks' permission
+#         it should contain the drink.long() data representation
+#     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+#         or appropriate status code indicating reason for failure
+# '''
+# @app.route('/drinks', methods=['POST'])
+# @requires_auth('post:drinks')
+# def post_drink(jwt):
+#     """Create a new Drink with the POST method"""
+#     if request.method != 'GET' and request.method != 'POST':
+#         abort(405)
+
+#     try:
+#         data = request.get_json()
+
+#         drink = Drink(
+#             title=data.get('title', None),
+#             recipe=json.dumps(data.get('recipe', None))
+#         )
+
+#         drink.insert()
+#     except Exception:
+#         db.session.rollback()
+#         abort(422)
+#     finally:
+#         return jsonify({
+#             'success': True,
+#             'drinks': [drink.long()]
+#         }), 200
+#         db.session.close()
+
+
+# '''
+# @TODO implement endpoint
+#     PATCH /drinks/<id>
+#         where <id> is the existing model id
+#         it should respond with a 404 error if <id> is not found
+#         it should update the corresponding row for <id>
+#         it should require the 'patch:drinks' permission
+#         it should contain the drink.long() data representation
+#     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+#         or appropriate status code indicating reason for failure
+# '''
+# @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+# @requires_auth('patch:drinks')
+# def patch_drink(jwt, drink_id):
+#     """Update a pre-existing Drink using the PATCH method"""
+#     data = request.get_json()
+
+#     drink = Drink.query.get(drink_id)
+
+#     if drink is None:
+#         abort(404, 'Drink not found')
+
+#     try:
+#         drink.title = data.get('title')
+#         drink.recipe = data.get('recipe')
+#         drink.update()
+#     except Exception:
+#         db.session.rollback()
+#         abort(422)
+#     finally:
+#         return jsonify({
+#             'success': True,
+#             'drinks': [drink.long()]
+#         }), 200
+#         db.session.close()
+
+
+# '''
+# @TODO implement endpoint
+#     DELETE /drinks/<id>
+#         where <id> is the existing model id
+#         it should respond with a 404 error if <id> is not found
+#         it should delete the corresponding row for <id>
+#         it should require the 'delete:drinks' permission
+#     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+#         or appropriate status code indicating reason for failure
+# '''
+# @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+# @requires_auth('delete:drinks')
+# def delete_drink(jwt, drink_id):
+#     """Delete an existing drink using the DELETE method"""
+#     drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+#     if drink is None:
+#         abort(404, 'Drink not found.')
+
+#     try:
+#         drink.delete()
+#     except Exception:
+#         db.session.rollback()
+#         abort(422)
+#     finally:
+#         return jsonify({
+#             'success': True,
+#             'delete': drink_id
+#         }), 200
+#         db.session.close()
 
 
 # Error Handling
@@ -145,3 +274,4 @@ def invalid_claims(error):
         "success": False,
         "error": 401,
         "message": error.__dict__
+    })
